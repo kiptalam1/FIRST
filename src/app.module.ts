3159e1114428a15a2eea6940a08c6d6dce301c88
adams <adamskiptalam0@gmail.com>
2026-06-21 20:07:04 +0300

    Controllers: resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { DogsController } from './dogs/dogs.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [CatsModule],
+  controllers: [AppController, DogsController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/dogs/dogs.controller.ts~
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, Res } from '@nestjs/common';
+import { response } from 'express';
+
+@Controller('dogs')
+export class DogsController {
+    @Get(@Res() response: Response)
+    findAll(): string {
+        return 'This action returns all dogs';
+    }
+    @Post()
+    create(): string {
+        return 'This action adds a new dog';
+    }
+}
