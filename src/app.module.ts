c0f1b6e44d35269167bb622989d887e0470c7554
adams <adamskiptalam0@gmail.com>
2026-06-21 22:31:57 +0300

    controllers end

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { DogsController } from './dogs/dogs.controller';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
-  imports: [CatsModule],
-  controllers: [AppController, DogsController],
-  providers: [AppService],
+    imports: [CatsModule],
+    controllers: [AppController, DogsController, CatsController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
