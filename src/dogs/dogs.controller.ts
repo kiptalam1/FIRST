05b8ea74852980539f76184f3a4f5f643a24e597
adams <adamskiptalam0@gmail.com>
2026-06-21 21:42:28 +0300

    controllers: Redirection

--- src/dogs/dogs.controller.ts
+++ src/dogs/dogs.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Header, HttpCode, Post, Res } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Post, Query, Redirect, Res } from '@nestjs/common';
 import { response } from 'express';
+import { version } from 'os';
 
 @Controller('dogs')
 export class DogsController {
@@ -13,8 +14,21 @@ export class DogsController {
     create(): string {
         return 'This action adds a new dog';
     }
+
     @Get('abdc/*')
+    // default status-code is 302 (Found)
+    @Redirect('https://nestjs.com', 301)
     findAllRoutes() {
-        return 'This rroute uses a wildcard';
+        return 'This route uses a wildcard';
     }
+
+    // Redirection
+    @Get('docs')
+    @Redirect('https://docs.nestjs.com', 302)
+    getDocs(@Query('version') version) {
+        if (version && version === '5') {
+            return { url: 'https://docs.nestjs.com/v5/' }
+        }
+    }
+
 }
