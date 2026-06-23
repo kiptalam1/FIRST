3e13a44cf4fd7f1044917b9f1e9bf4496db3613d
adams <adamskiptalam0@gmail.com>
2026-06-23 16:52:18 +0300

    Modules: Global modules

--- src/dogs/dogs.module.ts
+++ src/dogs/dogs.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { DogsController } from './dogs.controller';
 import { DogsService } from './dogs.service';
 
+@Global()
 @Module({
   controllers: [DogsController],
   providers: [DogsService],
